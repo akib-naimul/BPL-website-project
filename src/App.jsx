7ade9790b9f83e4aba6d1a9ba0eef6cf2ab77146
akib-naimul <akibnaimul2000@gmail.com>
2025-09-26 18:13:19 +0600

    cards layout title design

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,16 @@ function App() {
     <>
       
     <Navbar></Navbar>
+    <div className=' max-w-[1200px] mx-auto flex justify-between items-center'>
+      <h1 className='font-bold text-2xl'>Available Players</h1>
 
+       <div>
+        <button className='py-3 px-4 border-l border-grey-400 rounded-l-2xl border-r-0 bg-amber-950'>Selected <span>(0)</span></button>
+        <button className='py-3 px-4 border-l border-grey-400 rounded-l-2xl border-l-0'>Available</button>
+       </div>
+
+    </div>
+       
     <Suspense fallback={<span className="loading loading-bars loading-xl"></span>}>
       <AvailablePlayers playersPromise={playersPromise} ></AvailablePlayers>
     </Suspense>
