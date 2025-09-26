f7f36283764754c75b992d4f97163365d18d67ef
akib-naimul <akibnaimul2000@gmail.com>
2025-09-26 20:31:04 +0600

    Selected Players Dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
       
     <Navbar availableBalance={availableBalance}></Navbar>
     <div className=' max-w-[1200px] mx-auto flex justify-between items-center'>
-      <h1 className='font-bold text-2xl'>Available Players</h1>
+      <h1 className='font-bold text-2xl'>{toggle === true?"Available Players": `Selected Players(${purchasedPlayers.length}/6)`}</h1>
 
        <div>
         <button onClick={()=>setToggle(true)} className={`py-3 px-4 border-l border-grey-400 rounded-l-2xl border-r-0 ${toggle === true?" bg-amber-950":""}`}>Available <span>(0)</span></button>
