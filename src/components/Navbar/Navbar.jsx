55de7a27d90a60b0ac463923f388fee64d832c2e
akib-naimul <akibnaimul2000@gmail.com>
2025-09-26 09:55:13 +0600

    Nav bar created

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import navImg from "../../assets/logo.png"
+
+const Navbar = () => {
+    return (
+        <div>
+         <div className="navbar max-w-[1200px] mx-auto ">
+             <div className="flex-1">
+             <a className="text-xl"><img className='w-[60px] h-[60px]' src={navImg} alt="" /></a>
+    
+         </div>
+         <div className="flex-none">
+           <span className='mr-2'>600000000000000</span>
+           <span className='mr-2'>Coin</span>
+         </div>
+        </div>
+            
+        </div>
+    );
+};
+
+export default Navbar;
