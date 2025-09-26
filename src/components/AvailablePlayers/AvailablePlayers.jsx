422a6a21b05a330cdba1bea1ebaa79e57766ac01
akib-naimul <akibnaimul2000@gmail.com>
2025-09-26 17:43:29 +0600

    Grid players Layout design done

--- src/components/AvailablePlayers/AvailablePlayers.jsx
+++ src/components/AvailablePlayers/AvailablePlayers.jsx
@@ -3,8 +3,30 @@ import React, { use } from 'react';
 const AvailablePlayers = ({playersPromise}) => {
     const playerData = use(playersPromise)
     return (
-        <div>
-            <p>Available</p>
+        <div className='max-w-[1200px] mx-auto grid grid-cols-1 md:grid-cols-3 gap-4'>
+            {
+                playerData.map(player=><div className="card bg-base-100 shadow-sm p-4">
+  <figure>
+    <img className='w-full h-[300px] object-cover'
+      src={player.player_img}
+      alt="Shoes" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">{player.player_name}</h2>
+    
+    <div className='flex justify-between font-bold'><span>Rating</span>
+    <span>{player.rating}</span></div>
+
+    <div className='flex justify-between font-bold'><span>{player.batting_style}</span>
+    <span>{player.bowling_style}</span></div>
+    <div className="card-actions mt-4 flex justify-between items-center">
+        <p>Price: {player.price}</p>
+      <button className="btn ">Choose Player</button>
+    </div>
+  </div>
+             </div>)
+            }
+            
         </div>
     );
 };
